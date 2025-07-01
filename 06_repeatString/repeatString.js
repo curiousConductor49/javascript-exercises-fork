7d4ce5c7afb520c37681650218bac64fc71122eb
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-07-01 15:32:40 -0400

    Solve exercise and enable and pass all tests

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatNum) {
+    let repeatedString = string;
+    if (repeatNum !== 0 && repeatNum > 0) {
+        for (let i = 0; i < repeatNum - 1; i++) {
+            repeatedString += string;
+        }
+    } else if (repeatNum < 0) {
+        repeatedString = "ERROR";
+    } else {
+        repeatedString = "";
+    }
+    
+    return repeatedString;
 };
 
 // Do not edit below this line
