8dddd34fe2d84d04a82be19e8aaca9864543cbca
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-07-01 15:37:45 -0400

    Solve exercise and enable and pass all tests

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const strIntoArr = string.split("");
+    strIntoArr.reverse();
+    const splitStr = strIntoArr.join("");
+    return splitStr;
 };
 
 // Do not edit below this line
