b5658aaeaab96279541b316e9efcb3392aa0ff68
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-07-02 15:57:45 -0400

    Solve exercise and enable and pass all tests

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(firstInt, secondInt) {
+    let finalSum = 0;
 
+    if (firstInt < 0 || secondInt < 0) {
+        return "ERROR";
+    } else if (!Number.isInteger(firstInt) || !Number.isInteger(secondInt)) {
+        return "ERROR";
+    } 
+
+    const startVal = firstInt < secondInt ? firstInt : secondInt;
+    const endVal = startVal === firstInt ? secondInt : firstInt;
+    
+    for (let i = startVal; i <= endVal; i++) {
+        finalSum += i;
+    }
+
+    return finalSum;
 };
 
 // Do not edit below this line
