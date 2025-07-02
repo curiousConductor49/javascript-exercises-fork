84cb0fe07530ca039eb431251c1b8694d528d218
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-07-02 17:12:05 -0400

    Solve exercise and enable and pass all tests

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  const celsiusVal = ((degrees - 32) * 5/9);
+  if (celsiusVal === 0) {
+    return parseInt(celsiusVal);
+  } else {
+    return parseFloat(celsiusVal.toFixed(1));
+  }
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  const fahrenheitVal = (degrees * 9/5 + 32);
+  if (fahrenheitVal === 0) {
+    return parseInt(fahrenheitVal);
+  } else {
+    return parseFloat(fahrenheitVal.toFixed(1));
+  }
+  
 };
 
 // Do not edit below this line
