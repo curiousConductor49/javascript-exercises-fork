d826101b8814527125e62e4f825f61c1e9e335bd
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-08 18:35:32 -0400

    Solve exercise and enable and pass all tests

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const arr = str.split("");
+                
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === "!" || arr[i] === "." || arr[i] === ",") {
+            arr.splice(i, 1);
+        }
+    }
 
+    const cleanArr = arr.filter((el) => el !== " ");
+
+    let word = cleanArr.join("").toLowerCase();
+    let wordReverse = [...word].reverse().join("");
+
+    if (word === wordReverse) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
