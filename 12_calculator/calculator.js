aca0424431c8c31cf067169aa4077fe4416d8a2b
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-08 16:48:06 -0400

    Solve exercise and enable and pass all tests

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length === 0) {
+    return 0;
+    } else {
+    return arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    };
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (arr.length === 0) {
+    return 0;
+  } else {
+    return arr.reduce((accumulator, currentValue) => accumulator*currentValue, 1);
+  }
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+      let numArr = [num];
+      while (num > 1) {
+        numArr.push(num - 1);
+        num--;
+      }
+      return numArr.reduce((accumulator, currentValue) => accumulator*currentValue, 1);
+  }
 };
 
 // Do not edit below this line
