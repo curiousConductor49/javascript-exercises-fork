2049388ce8491e36fab71e637e68031cb2fce82c
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-11 16:37:49 -0400

    Solve exercise and enable and pass all tests

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const sortedPeople = arr.sort((a, b) => {
+        const currentYear = new Date();
+        let birthYearA = a.yearOfBirth;
+        let deathYearA = a.yearOfDeath || currentYear.getFullYear();
+        let birthYearB = b.yearOfBirth;
+        let deathYearB = b.yearOfDeath || currentYear.getFullYear();
 
+        return (birthYearA - deathYearA) - (birthYearB - deathYearB);
+    })
+
+    return sortedPeople[0].name;
 };
 
 // Do not edit below this line
